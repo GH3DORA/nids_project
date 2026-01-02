70e76ad585133db7189482537b240019a6c75bf7
GH3DORA <chubbysehaj@gmail.com>
2026-01-02 17:35:55 +0530

    added data normalisation

--- ml_detector.py
+++ ml_detector.py
@@ -1,57 +1,41 @@
 from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 TRAINING_LIMIT=100
-ANOMALY_THRESHOLD=0.0
+ANOMALY_THRESHOLD=-0.15
+
+scaler=StandardScaler()
 
 model=IsolationForest(
-    n_estimators=100,
-    contamination=0.05,
+    n_estimators=200,
+    contamination=0.03,
     random_state=42
 )
 
 model_trained=False
 training_data=[]
+
 def train_model():
     global model_trained
-    model.fit(training_data)
+    X=np.array([training_data])
+    X_scaled=scaler.fit_transform(X)
+    model.fit(X_scaled)
     model_trained=True
     print(" ===== [ML] Model trained succesfully. =====")
 
-def analyze_packet_ml(combined_features):
+def analyze_packet_ml(features):
     global training_data
+
     if not model_trained:
-        training_data.append(combined_features)
+        training_data.append(features)
         if len(training_data)>=TRAINING_LIMIT:
             train_model()
         return None
-    np_features=np.array([combined_features])
-    anomaly_score=model.decision_function(np_features)[0]
+    
+    X=np.array([features])
+    X_scaled=scaler(X)
+    anomaly_score=model.decision_function(X_scaled)[0]
     if anomaly_score<ANOMALY_THRESHOLD:
         return anomaly_score
     return None
-
-
-# ========== OLD FEATURE EXTRACTION, INDIVIDUAL PACKET BASED ==========
-# def extract_features(packet):
-#     if not packet.haslayer("IP"):
-#         return
-    
-#     packet_size=len(packet)
-#     protocol=packet["IP"].proto
-#     src_port=0
-#     dst_port=0
-    
-#     if (packet.haslayer("TCP")):
-#         src_port=packet["TCP"].sport
-#         dst_port=packet["TCP"].dport
-#     elif (packet.haslayer("UDP")):
-#         src_port=packet["UDP"].sport
-#         dst_port=packet["UDP"].dport
-
-#     return [
-#         packet_size,
-#         protocol,
-#         src_port,
-#         dst_port
-#     ]
